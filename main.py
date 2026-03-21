de80d2536a9e3052d7ee41e3baa2cba79700c232
skbays03 <shawnbays2003@gmail.com>
2026-03-20 19:14:41 -0500

    updated player win condition to 11 instead of 2

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def main():
                     ball = Ball(screen_width//2, screen_height//2, BALL_RADIUS) 
                     ball.draw(screen)
 
-        if player.score >= 2:
+        if player.score >= 11:
             win_menu = WinConditionMenu(
                 screen_width//4,
                 screen_height//4,
